a1130ec96cbd938f180f3436537be9a1fc49651c
mohammed-rashik-1403 <72401607+mohammed-rashik-1403@users.noreply.github.com>
2023-12-03 00:46:44 +0530

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,74 +1,44 @@
 import NextAuth from 'next-auth';
 import AzureADProvider from 'next-auth/providers/azure-ad';
-import GitHubProvider from 'next-auth/providers/github';
+import CognitoProvider from 'next-auth/providers/cognito';
 
-const env = process.env;
-
-async function refreshAccessToken(token) {
-  try {
-  
-     const url = "https://login.microsoftonline.com/c0c7ad79-cf13-4f39-8eeb-a1829aa096a6/oauth2/token"
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: `grant_type=refresh_token`
-      + `&client_secret=o3e8Q~VUhdOvQmgqXsiAcU7C~PGH-RRSY7Cl8bg~`
-      + `&refresh_token=0.AWYAea3HwBPPOU-O66GCmqCWpvRj5Yf3wyJNlnu6LloBqpJmAPk.AgABAAEAAAAtyolDObpQQ5VtlI4uGjEPAgDs_wUA9P--g9Zko-gTTfgWKOM23FdCAAvWSnQfgBw7pE1UhjZRaWvC77M4ZkrvjbH_NTkB_Jb7zKvHFXVeBtlwkGpAjlaFM_nnt8-ShV-q1DLRAZogk6IFWWYU13E3p-c4E52u4zFUnJGsOMpKcfS-bbgiSCIhGk1diB0SFUcX8nTevZ985QUGU-8mrbCyex-UJThl0Ts64SCWP_kwDX2HygGxpDEKMaxhzd2XZ1B8HnyQ9sVsJHHu04rbgu0zGqeuIqQ6XcJ31i3y2mJytqRETPKGznXYl9fdATJNJDJi1Qsjp6t0uhyaIIzeW-r1RbXCImGc-irAvJfw-0n9siRDDqVJyFs1NBeV6C7cK9kj46Pg-5qQ4W3nVYk2oNXeU1pL5bOorfTjDrCPFp-gjGI43FTfe0vzoUgZaGFXuphnZtmQmEiSR66ROdz8WRHmHrem9q1tj4pkTT287nDD4BSLKId0esQqutEptvmKBRF34TY5y2GlJ-_u5uD2c84McZQH8dTFHYf4RqKv-9gF2r98ailSk7UxPfbczlQfD01weWUsMk4Bp0gBjWhCVF92BXWrmC8QhocWezGga0-c5hudx2QNKL65tbOd-b4CD8ytaHucBbr8goL-mJejHhPNJyU_ZGcjPrYCX-Lym52Uf6ie9inkmOGkU7WuljtIXeSMcnuatbHfR77KBcPWNydKygF8OdwjMAlKmKO6Rzz2VbY0lmj2NbPbLrf_iev8W1nqLVdavIKDroAMF9qC6-NqngUKTUlktJj5ImmC3Smwert85yXqXVP1hk2rC078QQHPbP1U2I018EYtdjwNf6FzFsJvD8OlN5A3UV6W4XPxYUvo5nxW06AUCqZCsSblMzAbfmcqbY2UXP-yFTO_dzvIsXUXitV-bHy_fw`
-      + `&client_id=87e563f4-c3f7-4d22-967b-ba2e5a01aa92`
-    })
-    const refreshedTokens = await response.json()
-  
-    if (!response.ok) {
-      throw refreshedTokens
-    }
-
-    return {
-      ...token,
-      accessToken: refreshedTokens.access_token,
-      accessTokenExpires: Date.now() + refreshedTokens.expires_in * 1000,
-      refreshToken: refreshedTokens.refresh_token ?? token.refreshToken, // Fall back to old refresh token
-    }
-  } catch (error) {
-    console.log(error)
-
-    return {
-      ...token,
-      error: "RefreshAccessTokenError",
-    }
-  }
-}
-
-
-
+/*
+Counsellor SSO login config file
+NextAuth with AzureADProvider
+*/
 export const authOptions = {
   providers: [
-    GitHubProvider({
-      clientId: '6740bcd8b840387e2ac3',
-      clientSecret: 'e2e4c200a0e06232cee7343d3bc443288846da9c',
+    CognitoProvider({
+      clientId: `241g55s6j05sqhc60pfv57if7t`,
+      clientSecret: null,
+      issuer: `https://cognito-idp.ap-southeast-1.amazonaws.com/ap-southeast-1_JtDXYUi6b`,
+      idToken: true,
+      client: {
+        token_endpoint_auth_method: "none"
+      },
+        checks: ['nonce','pkce','none','state'],
+     
+    
     }),
     AzureADProvider({
-      clientId: `87e563f4-c3f7-4d22-967b-ba2e5a01aa92`,
-      clientSecret: `o3e8Q~VUhdOvQmgqXsiAcU7C~PGH-RRSY7Cl8bg~`,
+      clientId: `655d42ce-169d-45ed-88e0-7f23ac4ca837`,
+      clientSecret: `nda8Q~GUCaBdZsm_5avtQvhRWZx7d1yZmUNooaAh`,
       tenantId: `c0c7ad79-cf13-4f39-8eeb-a1829aa096a6`,
+      checks: 'both',
       authorization: {
         params: { scope: 'openid email profile User.Read  offline_access' },
       },
-    //   profileUrl: 'https://graph.microsoft.com/oidc/userinfo',
-    //   profile: (profile) => {
-    //     console.log('THE PROFIdddLE....................................................', profile)
-    //   },
       httpOptions: { timeout: 10000 },
     }),
     
   ],
-  secret : "QXmdh/3Y85F80suN0ukW72ll/K5tv7Ey8eky3DJJ5pM=",
- 
+  secret : "qJ8Zg4buf9bFLO2knTn40Tnqatfdm1BNVRQ0GFXCX6w=",
   callbacks: {
-    
     async jwt({ token, user, account }) {
-      
+       console.log("tokeeeeeeeeeeeeeeeeee",token)
+       console.log("tokeeeeeeaccounteeeeeeeeeeee",account)
+       console.log("tokeeeeeeeusereeeeeeeeeee",user)
+       return token;
       if (account && user) {
         return {
           accessToken: account.id_token,
@@ -79,25 +49,21 @@ export const authOptions = {
           user,
         };
       }
-      console.log("+++++++++++++++++++++++++")
-      console.log(user)
-      if(account?.id_token) {
-        const [header, payload, sig] = account.id_token.split('.')
-        const idToken = JSON.parse(Buffer.from(payload, 'base64').toString('utf8'))
+      // if(account?.id_token) {
+      //   const [header, payload, sig] = account.id_token.split('.')
+      //   const idToken = JSON.parse(Buffer.from(payload, 'base64').toString('utf8'))
   
-        token.roles = [...idToken.roles]
-      }
+      //   token.roles = [...idToken.roles]
+      // }
       if (Date.now() < token.accessTokenExpires - 100000 || 0) {
         return token;
       }
-      return token
-      console.log("account,calbback",Date.now())
-      return refreshAccessToken(token)
+      // return token.data;
+      return token;
     },
-   
-  
     async session({ session, token }) {
-      console.log("newwwwww",token)
+      console.log("async session",session)
+      console.log("async session token",token)
       if (session) {
         session.user = token.user;
         session.user = token.user;
@@ -106,7 +72,96 @@ export const authOptions = {
       }
       return session;
     },
+  async redirect({ url, baseUrl }) {
+    console.log("toppppppppppppppp",url)
+    console.log("topppppppppbaseeeeeeee",baseUrl)
+    url = `/india/student-essentials/?asm=true`;
+    // baseUrl = ``
+    console.log("newwwwurl",url)
+    console.log("newwwbaseUrlwurl",baseUrl)
+    return url.startsWith(baseUrl) ? url : baseUrl+'/india';
+    // return `http://localhost:3005/india/student-essentials/?asm=true`;
+    // Allows relative callback URLs
+    if (url.startsWith("/")){
+      console.log("first con22",`${baseUrl}${url}`)
+      return `${baseUrl}${url}`
+    } 
+    // Allows callback URLs on the same origin
+    else if (new URL(url).origin === baseUrl){
+      console.log("second con222",`${baseUrl}${url}`)
+      return url
+    } else{
+      console.log("lasttttttt")
+      // return `http://localhost:3005/india/student-essentials/?asm=true`;
+      return baseUrl
+    }
+    
+    return `https://d3jsaq6594du80.cloudfront.net/india/student-essentials`;
   },
+   
+  },
+  session: {
+    strategy: "jwt",
+  },
+  cookies: {
+    sessionToken: {
+      name: `next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        secure: true,
+        domain: '.d3jsaq6594du80.cloudfront.net'
+      }
+    },
+    callbackUrl: {
+      name: `next-auth.callback-url`,
+      options: {
+        httpOnly: true,
+        sameSite: 'none',
+        path: '/',
+        secure: true
+      }
+    },
+    csrfToken: {
+      name: 'next-auth.csrf-token',
+      options: {
+        httpOnly: true,
+        sameSite: 'none',
+        path: '/',
+        secure: true
+      }
+    },
+    pkceCodeVerifier: {
+      name: 'next-auth.pkce.code_verifier',
+      options: {
+        httpOnly: true,
+        sameSite: 'none',
+        path: '/',
+        secure: true
+      }
+    },
+    state: {
+      name: `next-auth.state`,
+      options: {
+        httpOnly: true,
+        sameSite: 'none',
+        path: "/",
+        secure: true,
+      },
+    },
+    nonce: {
+      name: `next-auth.nonce`,
+      options: {
+        httpOnly: true,
+        sameSite: "none",
+        path: "/",
+        secure: true,
+      },
+    },
+  },
+  
+ 
 };
 
 export default NextAuth(authOptions);
