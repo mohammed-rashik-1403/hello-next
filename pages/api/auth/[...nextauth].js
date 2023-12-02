a6771dfa815c42f2b382d725be0467ea392bf20f
mohammed-rashik-1403 <72401607+mohammed-rashik-1403@users.noreply.github.com>
2023-12-03 00:55:22 +0530

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -79,6 +79,7 @@ export const authOptions = {
     // baseUrl = ``
     console.log("newwwwurl",url)
     console.log("newwwbaseUrlwurl",baseUrl)
+    return baseUrl
     return url.startsWith(baseUrl) ? url : baseUrl+'/india';
     // return `http://localhost:3005/india/student-essentials/?asm=true`;
     // Allows relative callback URLs
