aca3fdeb0c0fcc3a72ca38178e7dd2bea5c791e0
mohammed-rashik-1403 <72401607+mohammed-rashik-1403@users.noreply.github.com>
2023-12-03 01:06:28 +0530

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -73,6 +73,7 @@ export const authOptions = {
       return session;
     },
   async redirect({ url, baseUrl }) {
+    return baseUrl;
     console.log("toppppppppppppppp",url)
     console.log("topppppppppbaseeeeeeee",baseUrl)
     url = `/india/student-essentials/?asm=true`;
